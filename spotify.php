432704fe8dc024e127cdb424107b5903981b85a1
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2019-11-29 02:04:32 +0700

    fix cf

--- spotify.php
+++ spotify.php
@@ -1,42 +1,47 @@
 <?php
-class Spotify {
+class Spotify
+{
     public $result;
     function __construct($jumlah)
     {
-        if(!is_numeric($jumlah)) {
+        if (!is_numeric($jumlah)) {
             throw new Exception('jumlah not numeric');
         }
         $jumlah = trim($jumlah);
         $result = file_get_contents("http://n1ghthax0r.000webhostapp.com/api/spotify/?jumlah={$jumlah}");
-        $this->result=$result;
+        $this->result = $result;
     }
 
     public function saveResult($fileName, $mode)
     {
-        $tulis=fopen($fileName, $mode);
-        if($tulis) {
+        $tulis = fopen($fileName, $mode);
+        if ($tulis) {
             fprintf($tulis, $this->result);
             fclose($tulis);
-        }
-        else {
+        } else {
             throw new Exception('failed to save result');
         }
     }
 }
-$banner=file_get_contents('banner.txt');
-fprintf(STDOUT ,"%s\n", $banner);
-$jumlah = readline('jumlah >> ');
-$generate = new Spotify($jumlah);
-$generate->saveResult('result.json','a');
+try {
+    $banner = file_get_contents('banner.txt');
+    fprintf(STDOUT, "%s\n", $banner);
+    $jumlah = readline('jumlah >> ');
+    $generate = new Spotify($jumlah);
+    $generate->saveResult('result.json', 'a');
 
-$json = json_decode($generate->result, true);
+    $json = json_decode($generate->result, true);
 
-foreach( (array)$json as $row) {
-    fprintf(STDOUT, "Type    :\t%s\n", $row['Account Type']);
-    fprintf(STDOUT, "Email   :\t%s\n", $row['Email']);
-    fprintf(STDOUT, "Pass    :\t%s\n", $row['Password']);
-    fprintf(STDOUT, "Country :\t%s\n", $row['Country']);
-    if(!empty($row['Expired'])) {
-        fprintf(STDOUT, "Expired :%s\n", $row['Expired']);
+    foreach ((array)$json as $row) {
+        fprintf(STDOUT, "Type    :\t%s\n", $row['Account Type']);
+        fprintf(STDOUT, "Email   :\t%s\n", $row['Email']);
+        fprintf(STDOUT, "Pass    :\t%s\n", $row['Password']);
+        fprintf(STDOUT, "Country :\t%s\n", $row['Country']);
+        if (!empty($row['Expired'])) {
+            fprintf(STDOUT, "Expired :%s\n", $row['Expired']);
+        }
     }
+} catch (Exception $e) {
+    fprintf(STDERR, "%s\n", $e->getMessage());
+    exit(1);
 }
